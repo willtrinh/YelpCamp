7f13073b8b8466144e2c116420cd0907e57f2e39
willtrinh <williamltrinh@gmail.com>
2020-10-28 14:59:26 -0700

    refactor app.js and routes/campgrounds.js

--- app.js
+++ app.js
@@ -1,45 +1,36 @@
-require('dotenv').config();
+require("dotenv").config();
 
-var express             = require("express"),
-    app                 = express(),
-    bodyParser          = require("body-parser"),
-    mongoose            = require("mongoose"),
-    passport            = require("passport"),
-    LocalStrategy       = require("passport-local"),
-    methodOverride      = require("method-override"),
-    flash               = require("connect-flash"),
-    //Models
-    Comment             = require("./models/comment"),
-    User                = require("./models/user"),
-    Campground          = require("./models/campground"),
-    //Seed
-    seedDB              = require("./seeds");
+var express = require("express"),
+  app = express(),
+  bodyParser = require("body-parser"),
+  mongoose = require("mongoose"),
+  passport = require("passport"),
+  LocalStrategy = require("passport-local"),
+  methodOverride = require("method-override"),
+  flash = require("connect-flash"),
+  //Models
+  Comment = require("./models/comment"),
+  User = require("./models/user"),
+  Campground = require("./models/campground"),
+  //Seed
+  seedDB = require("./seeds");
 
 // requiring routes
 var commentRoutes = require("./routes/comments"),
-    campgroundRoutes = require("./routes/campgrounds"),
-    userRoutes = require("./routes/users"),
-    indexRoutes = require("./routes/index");
-    
+  campgroundRoutes = require("./routes/campgrounds"),
+  userRoutes = require("./routes/users"),
+  indexRoutes = require("./routes/index");
+
 // config
-mongoose.set('useUnifiedTopology', true);
-mongoose.set('useFindAndModify', false);
-mongoose.set('useCreateIndex', true);
-mongoose.set('useNewUrlParser', true);
-
-<<<<<<< HEAD
+mongoose.set("useUnifiedTopology", true);
+mongoose.set("useFindAndModify", false);
+mongoose.set("useCreateIndex", true);
+mongoose.set("useNewUrlParser", true);
 
 var url = process.env.DATABASEURL || "mongodb://localhost:27017/yelp_camp";
 mongoose.connect(url);
-//mongoose.connect("mongodb://localhost:27017/yelp_camp", {useNewUrlParser: true});
-//mongoose.connect("mongodb+srv://dbuser:dbpass123@yelpcamp.otzjm.mongodb.net/yelp_camp?retryWrites=true&w=majority", {useNewUrlParser: true});
 
-=======
-var url = process.env.DATABASEURL || "mongodb://localhost:27017/yelp_camp";
-mongoose.connect(url);
->>>>>>> 1f8d0cef716d26c46fc4d343c80e67a019e27cd3
-
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
 app.use(methodOverride("_method"));
@@ -48,22 +39,24 @@ app.locals.moment = require("moment");
 //seedDB(); // Seed function
 
 // PASSPORT CONFIG
-app.use(require("express-session")({
+app.use(
+  require("express-session")({
     secret: "Once again this is a secret!",
     resave: false,
-    saveUninitialized: false
-}));
+    saveUninitialized: false,
+  })
+);
 app.use(passport.initialize());
 app.use(passport.session());
 passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
-app.use(function(req, res, next){
-    res.locals.currentUser = req.user;
-    res.locals.error = req.flash("error");
-    res.locals.success = req.flash("success");
-    next();
+app.use(function (req, res, next) {
+  res.locals.currentUser = req.user;
+  res.locals.error = req.flash("error");
+  res.locals.success = req.flash("success");
+  next();
 });
 
 app.use(indexRoutes);
@@ -71,11 +64,6 @@ app.use("/campgrounds", campgroundRoutes);
 app.use("/campgrounds/:id/comments", commentRoutes);
 app.use("/users", userRoutes);
 
-
-app.listen(process.env.PORT, process.env.IP, function(){
-    console.log("The YelpCamp Server has started!");
-<<<<<<< HEAD
+app.listen(process.env.PORT, process.env.IP, function () {
+  console.log("The YelpCamp Server has started!");
 });
-=======
-});
->>>>>>> 1f8d0cef716d26c46fc4d343c80e67a019e27cd3
